01f1f9b1653c2a5d2782b85c4d0c859ac48f9987
pylover <vahid.mardani@gmail.com>
2018-07-08 23:45:59 +0430

    Migrating to pytest

--- bddrest/tests/test_autodocumentation.py
+++ bddrest/tests/test_autodocumentation.py
@@ -3,7 +3,7 @@ import unittest
 from os import path
 import io
 
-from bddrest.authoring import given, then, response
+from bddrest.authoring import given, response
 
 
 def wsgi_application(environ, start_response):
@@ -11,33 +11,28 @@ def wsgi_application(environ, start_response):
     yield b'Nothing'
 
 
-class AutoDocumentationTestCase(unittest.TestCase):
+def test_autodoc_filename():
+    filename = tempfile.mktemp()
+    with given(
+        wsgi_application,
+        title='Testing auto documentation',
+        url='/apiv1/devices/name: SM-12345678',
+        autodoc=filename,
+    ):
+        assert response.status_code == 200
 
-    def test_autodoc_filename(self):
-        filename = tempfile.mktemp()
-        with given(
-            wsgi_application,
-            title='Testing auto documentation',
-            url='/apiv1/devices/name: SM-12345678',
-            autodoc=filename,
-        ):
-            then(response.status_code == 200)
-
-        self.assertTrue(path.exists(filename))
-
-    def test_autodoc_file_object(self):
-        file = io.StringIO()
-        with given(
-            wsgi_application,
-            title='Testing auto documentation',
-            url='/apiv1/devices/name: SM-12345678',
-            autodoc=file,
-        ):
-            then(response.status_code == 200)
-
-        self.assertTrue(len(file.getvalue()) > 0)
+    assert path.exists(filename)
 
 
-if __name__ == '__main__':  # pragma: no cover
-    unittest.main()
+def test_autodoc_file_object():
+    file = io.StringIO()
+    with given(
+        wsgi_application,
+        title='Testing auto documentation',
+        url='/apiv1/devices/name: SM-12345678',
+        autodoc=file,
+    ):
+        assert response.status_code == 200
+
+    assert len(file.getvalue()) > 0
 
