83da21fd4c6d51f67b4dfe50cb98fecc49784d83
pylover <vahid.mardani@gmail.com>
2018-07-10 10:33:06 +0430

    Migrating to pytest

--- bddrest/tests/test_headers.py
+++ bddrest/tests/test_headers.py
@@ -4,92 +4,86 @@ import re
 from bddrest.specification.headerset import HeaderSet
 
 
-class HeaderSetTestCase(unittest.TestCase):
+def test_constructor():
+    expected_headers = [('A', 'B'), ('C', 'D')]
+    headers = HeaderSet(('A: B', 'C: D'))
+    assert expected_headers == headers
 
-    def test_constructor(self):
-        expected_headers = [('A', 'B'), ('C', 'D')]
-        headers = HeaderSet(('A: B', 'C: D'))
-        self.assertListEqual(expected_headers, headers)
+    headers = HeaderSet({'A': 'B', 'C': 'D'})
+    assert expected_headers == headers
 
-        headers = HeaderSet({'A': 'B', 'C': 'D'})
-        self.assertListEqual(expected_headers, headers)
+def test_append():
+    expected_headers = [('A', 'B'), ('C', 'D')]
+    headers = HeaderSet()
+    headers.append(('A', 'B'))
+    headers.append(('C', 'D'))
+    assert expected_headers == headers
 
-    def test_append(self):
-        expected_headers = [('A', 'B'), ('C', 'D')]
-        headers = HeaderSet()
-        headers.append(('A', 'B'))
-        headers.append(('C', 'D'))
-        self.assertListEqual(expected_headers, headers)
+    headers = HeaderSet()
+    headers.append('A: B')
+    headers.append('C: D')
+    assert expected_headers == headers
 
-        headers = HeaderSet()
-        headers.append('A: B')
-        headers.append('C: D')
-        self.assertListEqual(expected_headers, headers)
+    headers = HeaderSet()
+    headers.append('A', 'B')
+    headers.append('C', 'D')
+    assert expected_headers == headers
 
-        headers = HeaderSet()
-        headers.append('A', 'B')
-        headers.append('C', 'D')
-        self.assertListEqual(expected_headers, headers)
+def test_insert():
+    expected_headers = [('A', 'B'), ('C', 'D')]
+    headers = HeaderSet()
+    headers.insert(0, ('C', 'D'))
+    headers.insert(0, ('A', 'B'))
+    assert expected_headers == headers
 
-    def test_insert(self):
-        expected_headers = [('A', 'B'), ('C', 'D')]
-        headers = HeaderSet()
-        headers.insert(0, ('C', 'D'))
-        headers.insert(0, ('A', 'B'))
-        self.assertListEqual(expected_headers, headers)
+    headers = HeaderSet()
+    headers.insert(0, 'C: D')
+    headers.insert(0, 'A: B')
+    assert expected_headers == headers
 
-        headers = HeaderSet()
-        headers.insert(0, 'C: D')
-        headers.insert(0, 'A: B')
-        self.assertListEqual(expected_headers, headers)
+    headers = HeaderSet()
+    headers.insert(0, 'C', 'D')
+    headers.insert(0, 'A', 'B')
+    assert expected_headers == headers
 
-        headers = HeaderSet()
-        headers.insert(0, 'C', 'D')
-        headers.insert(0, 'A', 'B')
-        self.assertListEqual(expected_headers, headers)
+def test_setitem_getitem_delitem():
+    expected_headers = [('A', 'B'), ('C', 'D')]
+    # setitem
+    headers = HeaderSet(['A: F'])
+    headers[0] = 'A: B'
+    headers['C'] = 'D'
+    assert expected_headers == headers
 
-    def test_setitem_getitem_delitem(self):
-        expected_headers = [('A', 'B'), ('C', 'D')]
-        # setitem
-        headers = HeaderSet(['A: F'])
-        headers[0] = 'A: B'
-        headers['C'] = 'D'
-        self.assertListEqual(expected_headers, headers)
+    # getitem
+    assert expected_headers[0] == headers[0]
+    assert 'B' == headers['A']
+    assert 'B' == headers['a']
 
-        # getitem
-        self.assertEqual(expected_headers[0], headers[0])
-        self.assertEqual('B', headers['A'])
-        self.assertEqual('B', headers['a'])
+    # delitem
+    del headers[0]
+    del headers['C']
+    assert [] == headers
 
-        # delitem
-        del headers[0]
-        del headers['C']
-        self.assertListEqual([], headers)
+def test_in_operator():
+    headers = HeaderSet(['A: F'])
+    assert 'A' in headers
+    assert 'a' in headers
+    assert 'A: F' in headers
 
-    def test_in_operator(self):
-        headers = HeaderSet(['A: F'])
-        self.assertIn('A', headers)
-        self.assertIn('a', headers)
-        self.assertIn('A: F', headers)
+def test_extend():
+    expected_headers = [('A', 'B'), ('C', 'D')]
 
-    def test_extend(self):
-        expected_headers = [('A', 'B'), ('C', 'D')]
+    headers = HeaderSet(['A: B'])
+    headers.extend(['C: D'])
+    assert expected_headers == headers
 
-        headers = HeaderSet(['A: B'])
-        headers.extend(['C: D'])
-        self.assertListEqual(expected_headers, headers)
+def test_regex_match():
+    headers = HeaderSet(['Content-Type: application/json;utf-8'])
+    assert re.compile('^content-type: .*', re.I) in headers
 
-    def test_regex_match(self):
-        headers = HeaderSet(['Content-Type: application/json;utf-8'])
-        self.assertIn(re.compile('^content-type: .*', re.I), headers)
-
-    def test_remove(self):
-        headers = HeaderSet([('A', 'B'), ('C', 'D')])
-        headers.remove('A')
-        headers.remove('C: D')
-        self.assertListEqual([], headers)
-
-
-if __name__ == '__main__':  # pragma: no cover
-    unittest.main()
+def test_remove():
+    headers = HeaderSet([('A', 'B'), ('C', 'D')])
+    headers.remove('A')
+    headers.remove('C: D')
+    assert [] == headers
 
