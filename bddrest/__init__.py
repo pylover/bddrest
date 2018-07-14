0c837be49d0acc972bb922b210677357024569b5
pylover <vahid.mardani@gmail.com>
2018-07-15 01:09:41 +0430

    v1.10.1a6

--- bddrest/__init__.py
+++ bddrest/__init__.py
@@ -1,9 +1,9 @@
 
 from .specification import Call, FirstCall, AlteredCall, HeaderSet, HTTPStatus
 from .authoring import Given, when, story, response, Story, Append, Update,\
-    Remove
+    Remove, status
 from .exceptions import InvalidUrlParametersError, CallVerifyError
 
 
-__version__ = '1.10.0a6'
+__version__ = '1.10.1a6'
 
