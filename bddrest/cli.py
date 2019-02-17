ca9ad957d3a6376856a8a2f54f3bf0f09cbfafc5
mohadese-yousefi <45990057+mohadese-yousefi@users.noreply.github.com>
2019-02-17 12:09:15 +0330

    Migrated to easycli (#51)
    * added easycli moudal to bddrest
    
    * Edited import order!
    
    * Deleted def main

---
+++ bddrest/cli.py
@@ -0,0 +1,12 @@
+from easycli import Root
+
+from .documentary import DocumentaryLauncher
+
+
+class Main(Root):
+    __help__ = 'bddrest'
+    __completion__ = True
+    __arguments__ = [
+        DocumentaryLauncher
+    ]
+
