eda6bb2f7c3038e50f8016297d5ccabb97d285a6
mohadese-yousefi <45990057+mohadese-yousefi@users.noreply.github.com>
2019-02-17 12:09:15 +0330

    Migrated to easycli (#51)
    * added easycli moudal to bddrest
    
    * Edited import order!
    
    * Deleted def main

--- bddrest/cli.py
+++ bddrest/cli.py
@@ -1,13 +1,20 @@
 from easycli import Root
 
 from .documentary import DocumentaryLauncher
+<<<<<<< HEAD
 from .mockupserver import MockupServer
+=======
+>>>>>>> Migrated to easycli (#51)
 
 class Main(Root):
     __help__ = 'bddrest'
     __completion__ = True
     __arguments__ = [
+<<<<<<< HEAD
         DocumentaryLauncher,
         MockupServer,
+=======
+        DocumentaryLauncher
+>>>>>>> Migrated to easycli (#51)
     ]
 
