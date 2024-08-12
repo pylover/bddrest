3b8444e81c9e48a8fdbad16b69398ce484f774ba
pylover <vahid.mardani@gmail.com>
2024-08-13 03:00:45 +0330

    Enhancement: Additional tests for querystring

---
+++ tests/test_querystring.py
@@ -0,0 +1,40 @@
+import cgi
+import json
+import tempfile
+
+import pytest
+
+from bddrest import Given, when, response
+
+
+def wsgi_application(environ, start_response):
+    resp = environ.get('QUERY_STRING')
+    start_response('200 OK', [
+        ('Content-Type', 'text/plain;charset=utf-8'),
+        ('Content-Length', str(0 if resp is None else len(resp))),
+    ])
+
+    if resp is not None:
+        yield resp
+
+
+def test_querystring():
+    with Given(wsgi_application, query=dict(foo='')) as s:
+        assert response.status == 200
+        assert response.text == 'foo='
+
+        when(query='foo=bar')
+        assert response.status == 200
+        assert response.text == 'foo=bar'
+
+        when(query='foo=')
+        assert response.status == 200
+        assert response.text == ''
+
+        when(url='/?foo=')
+        assert response.status == 200
+        assert response.text == 'foo='
+
+        when(query=dict(foo=''))
+        assert response.status == 200
+        assert response.text == 'foo='
