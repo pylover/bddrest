47c6dd50078fbaa8f4bb8f792db9b9f18bc41dc3
pylover <vahid.mardani@gmail.com>
2024-08-16 20:49:53 +0330

    v4.3.0

--- bddrest/__init__.py
+++ bddrest/__init__.py
@@ -6,4 +6,4 @@ from .authoring import Given, when, story, response, Story, Append, Update, \
 from .exceptions import InvalidUrlParametersError, CallVerifyError
 
 
-__version__ = '4.2.6'
+__version__ = '4.3.0'
